38cd785dd1cf191f64325c133eb20af374c13350
Hao-Zhuang-HNU <2867826220@qq.com>
2026-03-01 22:06:02 +0800

    upload old train code named TSR_train_RefKV.py

--- train_localGT.sh
+++ train_localGT.sh
@@ -13,7 +13,7 @@ export PYTORCH_CUDA_ALLOC_CONF=backend:cudaMallocAsync
 # If you want to revert to native caching allocator:
 # export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:1024,garbage_collection_threshold:0.8
 
-python -m torch.distributed.run --nproc_per_node=4 Geo_train.py \
+python -m torch.distributed.run --nproc_per_node=2 Geo_train.py \
     --config_path ./config/config_Geo_localGT.yml \
     --dist \
-    --GPU_ids 0,1,2,3
+    --GPU_ids 0,1
