9e59401a333677670d3f9dcadeb24592c5d24547
Hao-Zhuang-HNU <2867826220@qq.com>
2026-03-02 17:18:55 +0800

    delete withSeq.py

--- train_localGT.sh
+++ train_localGT.sh
@@ -13,7 +13,8 @@ export PYTORCH_CUDA_ALLOC_CONF=backend:cudaMallocAsync
 # If you want to revert to native caching allocator:
 # export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:1024,garbage_collection_threshold:0.8
 
-python -m torch.distributed.run --nproc_per_node=2 Geo_train.py \
+python -m torch.distributed.run --nproc_per_node=3 Geo_train.py \
     --config_path ./config/config_Geo_localGT.yml \
     --dist \
-    --GPU_ids 0,1
+    --check_ref_frame \
+    --GPU_ids 0,1,2
